065f13c384c034beeabceeec6a188073ca13815a
code-sparrow <code.sparrow7@gmail.com>
2021-12-26 21:02:38 -0500

    finished exerciese 2, animation

--- 2AnimateIt/sketch.js
+++ 2AnimateIt/sketch.js
@@ -1,20 +1,33 @@
-let x = 600;
-let y = 300;
+let x = 800;
+let y = 500;
 let side = 60;
+let xPos = 0;
+let dx = 1;
 
 function setup() {
     createCanvas(x, y);
+    frameRate(60);
 }
 
 function draw() {
+    background(220);
     strokeWeight(2);
     stroke(51);
     
-    line(x/2+side/2, side/2, side/2, y/2+side/2);
+    xPos += dx;
+    
+    if (xPos >= x - side){
+        dx *= -1;
+    }
+    if (xPos <= 0){
+        dx *= -1;
+    }
+    
+    line(x/2, side/2, xPos+side/2, y/2);
     
     fill('red');
-    rect(x/2, 0, side, side);
+    rect(x/2-side/2, 0, side, side);
     
     fill('blue');
-    rect(0, y/2, side, side);
+    rect(xPos, y/2-side/2, side, side);
 }
